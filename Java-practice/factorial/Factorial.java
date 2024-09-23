2995d554b031ff14e119e6bd8c540b9ec5ce6694
DamianDubiejko <105378686+DamianDubiejko@users.noreply.github.com>
2024-09-23 17:29:58 +0200

    Add files via upload

---
+++ Java-practice/factorial/Factorial.java
@@ -0,0 +1,27 @@
+// Java program to find factorial 
+// of given number 
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = s.nextInt();
+        s.close(); // Closing the Scanner
+
+        String fact = factorial(n);
+        System.out.println("Factorial is " + fact);
+    }
+
+    // Method to compute factorial
+    public static String factorial(int n) {
+        BigInteger fact = BigInteger.ONE; // Use BigInteger.ONE instead of "new BigInteger('1')"
+        for (int i = 1; i <= n; i++) {
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        return fact.toString();
+    }
+}
